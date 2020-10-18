3071cf2410e9e6ee1a9d6cf4d9afc57456902ae9
Ndah28 <wahyuindah100@gmail.com>
2020-10-18 13:16:05 +0700

    Tugas Pertemuan 7

---
+++ VR Pertemuan 7/Assets/Scipt/HalamanUtama.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+    //method transisi
+    public void PindahHalaman(string halaman){
+        SceneManager.LoadScene(halaman);
+    }
+
+    //method keluar aplikasi
+    public void KeluarAplikasi(){
+        Application.Quit();
+    }
+}
